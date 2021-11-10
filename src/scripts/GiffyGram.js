2d9d5d51b7c811db4461d68fe6ca2f141feeb775
Chris Mccrary <chris.mccrary@asurion.com>
2021-11-10 14:15:50 -0600

    buttons are functional and html renders successfully

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,10 +1,12 @@
 import { PostList } from "./feed/PostList.js"
+import { PostEntry } from "./feed/PostEntry.js"
 
 export const GiffyGram = () => {
 
 
     let postsHTML =`
         <h1>Giffygram</h1>
+        ${PostEntry()}
         <section class='postList'>
             ${
                 PostList()
--- src/scripts/feed/PostEntry.js
+++ src/scripts/feed/PostEntry.js
@@ -1,12 +1,11 @@
 import { sendPostMessage } from "../data/provider.js";
 
-const applicationElement = document.querySelector("#giffygram")
 
-applicationElement.addEventListener("click", clickEvent => {
+document.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "newPost_submit") {
         const userPostTitle = document.querySelector("input[name='postTitle']").value
         const userPostURL = document.querySelector("input[name='postURL']").value
-        const userPostDescription = document.querySelector("input[name='postDescription']").value
+        const userPostDescription = document.querySelector("textarea[name='postDescription']").value
         const timestampe = new Date()
         const userIde = parseInt(localStorage.getItem("gg_user"))
 
@@ -17,25 +16,31 @@ applicationElement.addEventListener("click", clickEvent => {
             timestamp: timestampe,
             userId: userIde
         }
-
         sendPostMessage(dataToSendToAPI)
     }
+    if (clickEvent.target.id === "newPost_cancel") {
+        document.querySelector("input[name='postTitle']").value = ""
+        document.querySelector("input[name='postURL']").value = ""
+        document.querySelector("textarea[name='postDescription']").value = ""
+    }
 } )
+
+
 
 export const PostEntry = () => {
     return `        
         <div class="newPost">
             <div>                    
-                <input value= name="postTitle" class="newPost__input" type="text" autofocus placeholder="Title"/>
+                <input name="postTitle" class="newPost__input" type="text" autofocus placeholder="Title"/>
             </div>
             <div>
-                <input value= name="postURL" class="newPost__input" type="text" placeholder="URL of gif"/>
+                <input name="postURL" class="newPost__input" type="text" placeholder="URL of gif"/>
             </div>
             <textarea name="postDescription" class="newPost__input" placeholder="Story behind your gif..."></textarea>
             
             <button id="newPost_submit">Save</button>
             <button id="newPost_cancel">Cancel</button>
         </div>
-        <div class="miniMode">Have a gif to post?></div>
-    `
-}
+        `
+    }
+    //<div class="miniMode">Have a gif to post?</div>
